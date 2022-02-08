7155e86cdd1f91dc0e80d4e2eaad41cf529a36e7
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-02-08 13:18:21 -0300

    Initial Commit

---
+++ Rick and Morty Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty Challenge
+//
+//  Created by Felipe Brigagão de Almeida on 08/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
